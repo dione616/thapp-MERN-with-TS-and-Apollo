ec9fb9fba5186b68cb1f0f000a2a624fdd902ce9
dione616 <Vetal160199@i.ua>
2020-10-01 19:04:16 +0400

    Initial Commit with express

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 9000
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(port, () => {
+  console.log(`[app] : http://localhost:${port}`)
+})

3937c2afffb994e94d19ec68ff9280ae34586767
dione616 <Vetal160199@i.ua>
2020-10-01 19:28:38 +0400

    TS added to devDep and @types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-const express = require("express")
+import express from "express"
 const app = express()
 const port = 9000
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>Hello World</h1>")
 })
 
 app.listen(port, () => {

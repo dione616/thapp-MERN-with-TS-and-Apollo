7e56005a32564c404fdbc6b7c00fd493b77e7291
dione616 <Vetal160199@i.ua>
2020-10-02 16:40:55 +0400

    Second variant of creating Schemas in GraphQL

--- server/src/graphql/graphql.ts
+++ server/src/graphql/graphql.ts
@@ -7,7 +7,7 @@ import {
   GraphQLSchema,
   GraphQLList,
 } from "graphql"
-import { listings } from "./listings"
+import { listings } from "../listings"
 
 const Listing = new GraphQLObjectType({
   name: "Listing",

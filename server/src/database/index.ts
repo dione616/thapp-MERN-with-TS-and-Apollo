22ea10058bb88cd68c9a8523317e034d4d3be2a0
dione616 <Vetal160199@i.ua>
2020-10-03 22:13:35 +0400

    Mongo setup and then connected

---
+++ server/src/database/index.ts
@@ -0,0 +1,17 @@
+require("dotenv").config()
+import { MongoClient } from "mongodb"
+import { Database } from "../lib/types"
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n5ags.mongodb.net/<dbname>?retryWrites=true&w=majority`
+
+console.log(url)
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  const db = client.db("main")
+
+  return {
+    listings: db.collection("test_listings"),
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
-import express from "express"
+require("dotenv").config()
+/* import dotenv from "dotenv"
+dotenv.config() */
+
+import express, { Application } from "express"
 import { ApolloServer } from "apollo-server-express"
+import { connectDatabase } from "./database"
 import { typeDefs, resolvers } from "./graphql"
 
-const app = express()
-const port = 9000
+const mount = async (app: Application) => {
+  const db = await connectDatabase() //connect db
+  const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) })
+  server.applyMiddleware({ app, path: "/api" }) //endpoint for graphql
 
-const server = new ApolloServer({ typeDefs, resolvers })
-server.applyMiddleware({ app, path: "/api" }) //endpoint for graphql
+  app.listen(process.env.PORT, () => {
+    console.log(`[app] : http://localhost:${process.env.PORT}`)
+  })
 
-app.listen(port, () => {
-  console.log(`[app] : http://localhost:${port}`)
-})
+  const listings = await db.listings.find({}).toArray()
+  console.log(listings)
+}
+mount(express())

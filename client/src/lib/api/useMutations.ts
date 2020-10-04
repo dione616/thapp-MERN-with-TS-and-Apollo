90f88ee1e6dfec6dc042a98d1124f3e05426efe9
dione616 <Vetal160199@i.ua>
2020-10-04 17:00:27 +0400

    useReducer Hook

--- client/src/lib/api/useMutations.ts
+++ client/src/lib/api/useMutations.ts
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useReducer } from "react"
 import { server } from "./server"
 
 interface State<TData> {
@@ -10,24 +10,47 @@ interface State<TData> {
 //return type
 type MutationTuple<TData, TVariables> = [(varibles?: TVariables | undefined) => Promise<void>, State<TData>]
 
+//<-- Reducer --> //
+type Action<TData> = { type: "DELETE" } | { type: "DELETE_SUCCESS"; payload: TData } | { type: "DELETE_ERROR" }
+
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>) => {
+  switch (action.type) {
+    case "DELETE": {
+      return { ...state, loading: true }
+    }
+    case "DELETE_SUCCESS": {
+      return { data: action.payload, loading: false, error: false }
+    }
+    case "DELETE_ERROR": {
+      return { ...state, loading: false, error: true }
+    }
+    default:
+      return state
+  }
+}
+
 export const useMutation = <TData = any, TVariables = any>(query: string): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({ data: null, loading: false, error: false })
+  const fetchReducer = reducer<TData>()
+
+  const [state, dispatch] = useReducer(fetchReducer, { data: null, loading: false, error: false })
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({ data: null, loading: true, error: false })
+      dispatch({ type: "DELETE" })
 
       const { data, errors } = await server.fetch<TData, TVariables>({ query, variables })
 
       if (errors && errors.length) {
         throw new Error(errors[0].message)
       }
-      setState({ data: data, loading: false, error: false })
+      dispatch({ type: "DELETE_SUCCESS", payload: data })
     } catch (err) {
-      setState({ data: null, loading: false, error: true })
+      dispatch({ type: "DELETE_ERROR" })
       throw console.error(err) //prevents any code after this executes
     }
   }
 
   return [fetch, state]
 }
+
+//TODO: extrapolate to sharing reducer
--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react"
+import { useReducer, useEffect, useCallback } from "react"
 import { server } from "./server"
 
 interface State<TData> {
@@ -11,8 +11,27 @@ interface QueryResult<TData> extends State<TData> {
   refetch: () => void
 }
 
+//<--useReducer --> //
+type Action<TData> = { type: "FETCH" } | { type: "FETCH_SUCCESS"; payload: TData } | { type: "FETCH_ERROR" }
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>): State<TData> => {
+  switch (action.type) {
+    case "FETCH": {
+      return { ...state, loading: true }
+    }
+    case "FETCH_SUCCESS": {
+      return { data: action.payload, loading: false, error: false }
+    }
+    case "FETCH_ERROR": {
+      return { ...state, loading: false, error: true }
+    }
+    default:
+      return state
+  }
+}
+
 export const useQuery = <TData = any>(query: string): QueryResult<TData> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>()
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false,
@@ -21,16 +40,18 @@ export const useQuery = <TData = any>(query: string): QueryResult<TData> => {
   const fetch = useCallback(() => {
     const fetchApi = async () => {
       try {
-        setState({ data: null, loading: true, error: false })
+        dispatch({ type: "FETCH" })
 
         const { data, errors } = await server.fetch<TData>({ query })
 
         if (errors && errors.length) {
           throw new Error(errors[0].message)
         }
-        setState({ data, loading: false, error: false })
+
+        dispatch({ type: "FETCH_SUCCESS", payload: data })
       } catch (err) {
-        setState({ data: null, loading: false, error: true })
+        //request made but graphQL api returns error
+        dispatch({ type: "FETCH_ERROR" })
         throw console.error(err)
       }
     }

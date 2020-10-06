a36f73be46fe13ed0f27501b2d4ea980e2c39d67
dione616 <Vetal160199@i.ua>
2020-10-06 23:23:57 +0400

    Routing and new graphql types for data

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,33 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import * as serviceWorker from "./serviceWorker"
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "react-apollo"
-import { Listings } from "./sections"
+import { Listings, Home, Host, Listing, NotFound, User } from "./sections"
 import "./styles/index.css"
 
 const client = new ApolloClient({ uri: "/api" })
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        //? means optional
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Listings title="Available Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 )
--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,6 +1,6 @@
 require("dotenv").config()
 import { MongoClient } from "mongodb"
-import { Database } from "../lib/types"
+import { Database, User, Listing, Booking } from "../lib/types"
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n5ags.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
@@ -10,6 +10,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main")
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   }
 }
